2bda8aa10619e265c31cab2abc5fc8d90fd0cf05
kimdongyeon <kimyeon30@naver.com>
2024-03-14 21:58:52 +0900

    first

---
+++ Name_StudentID/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void)
+{
+	string name;
+	int studentID;
+
+	cout << "이름과 학번을 입력하세요" << endl;
+	cin >> name >> studentID;
+
+	cout << name << " " << studentID << endl;
+}
